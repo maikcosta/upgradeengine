40f1aa6a9cbc904bd4fe52e1c78e1f9d7b0b6ea8
Maik Costa <maikcosta@gmail.com>
2024-05-15 09:40:28 -0300

    Primeiro Commit

---
+++ src/main/java/com/upgradeengine/UpgradeengineApplication.java
@@ -0,0 +1,13 @@
+package com.upgradeengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpgradeengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpgradeengineApplication.class, args);
+	}
+
+}
